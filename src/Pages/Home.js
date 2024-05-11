5a127b0bf6676e2ec6163a61ec89dc6800469a0c
Hardik Sharma <sharmahardik1603@gmail.com>
2024-05-11 21:58:44 +0530

    pocket notes

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from '../Components/SideBarAndMessageArea';
+
+const Home = () => {
+  return (
+    <div style={{ display: 'flex' }}>
+      <HomePage />
+      {/* <SideBarAndMessageArea /> */}
+      
+    </div>
+  );
+};
+
+export default Home;
+ 
